gildas-ld/adminer

Add icons to action links

adminer/include/editing.inc.php
@@ -317,7 +317,7 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", $foreign
 		<?php echo (support("comment") ? "<td id='label-comment' $comment_class>" . lang('Comment') . "</td>" : ""); ?>
 	<?php } ?>
 	<td><?php
-		echo "<button name='add[" . (support("move_col") ? 0 : count($fields)) . "]' value='1' title='" . h(lang('Add next')) . "' class='light'><svg class='icon'><use href='static/icons.svg#add'/></svg></button>",
+		echo "<button name='add[" . (support("move_col") ? 0 : count($fields)) . "]' value='1' title='" . h(lang('Add next')) . "' class='light'>", icon("add"), "</button>",
 			script("row_count = " . count($fields) . ";");
 	?></td>
 </tr></thead>
@@ -333,7 +333,7 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", $foreign
 		echo "<tr $style>\n";
 
 		if (support("move_col")) {
-			echo "<th class='handle jsonly'><svg class='jsonly icon'><use href='static/icons.svg#handle'/></svg></th>";
+			echo "<th class='handle jsonly'>", icon("handle"), "</th>";
 		}
 		if ($type == "PROCEDURE") {
 			echo "<td>", html_select("fields[$i][inout]", explode("|", $inout), $field["inout"]), "</td>\n";
@@ -368,12 +368,12 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", $foreign
 
 		echo "<td>";
 		if (support("move_col")) {
-			echo "<button name='add[$i]' value='1' title='" . h(lang('Add next')) . "' class='light'><svg class='icon'><use href='static/icons.svg#add'/></svg></button>",
-				"<button name='up[$i]' value='1' title='" . h(lang('Move up')) . "' class='hidden light'><svg class='icon'><use href='static/icons.svg#arrow-up'/></svg></button>",
-				"<button name='down[$i]' value='1' title='" . h(lang('Move down')) . "' class='hidden light'><svg class='icon'><use href='static/icons.svg#arrow-down'/></svg></button>";
+			echo "<button name='add[$i]' value='1' title='" . h(lang('Add next')) . "' class='light'>", icon("add"), "</button>",
+				"<button name='up[$i]' value='1' title='" . h(lang('Move up')) . "' class='hidden light'>", icon("arrow-up"), "</button>",
+				"<button name='down[$i]' value='1' title='" . h(lang('Move down')) . "' class='hidden light'>", icon("arrow-down"), "</button>";
 		}
 		if ($orig == "" || support("drop_col")) {
-			echo "<button name='drop_col[$i]' value='1' title='" . h(lang('Remove')) . "' class='light'><svg class='icon'><use href='static/icons.svg#remove'/></svg></button>";
+			echo "<button name='drop_col[$i]' value='1' title='" . h(lang('Remove')) . "' class='light'>", icon("remove"), "</button>";
 		}
 		echo "</td>\n</tr>\n";
 	}

adminneo-org/adminneo

adminer/include/editing.inc.php
@@ -382,7 +382,7 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", $foreign
 		<?php echo (support("comment") ? "<td id='label-comment' $comment_class>" . lang('Comment') . "</td>" : ""); ?>
 	<?php } ?>
 	<td><?php
-		echo "<button name='add[" . (support("move_col") ? 0 : count($fields)) . "]' value='1' title='" . h(lang('Add next')) . "' class='light'><svg class='icon'><use href='static/icons.svg#add'/></svg></button>",
+		echo "<button name='add[" . (support("move_col") ? 0 : count($fields)) . "]' value='1' title='" . h(lang('Add next')) . "' class='light'>", icon("add"), "</button>",
 			script("row_count = " . count($fields) . ";");
 	?></td>
 </tr></thead>
@@ -398,7 +398,7 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", $foreign
 		echo "<tr $style>\n";
 
 		if (support("move_col")) {
-			echo "<td class='handle jsonly'><svg class='jsonly icon'><use href='static/icons.svg#handle'/></svg></td>";
+			echo "<td class='handle jsonly'>", icon("handle"), "</td>";
 		}
 		if ($type == "PROCEDURE") {
 			echo "<td>", html_select("fields[$i][inout]", explode("|", $inout), $field["inout"]), "</td>\n";
@@ -433,12 +433,12 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", $foreign
 
 		echo "<td>";
 		if (support("move_col")) {
-			echo "<button name='add[$i]' value='1' title='" . h(lang('Add next')) . "' class='light'><svg class='icon'><use href='static/icons.svg#add'/></svg></button>",
-				"<button name='up[$i]' value='1' title='" . h(lang('Move up')) . "' class='hidden light'><svg class='icon'><use href='static/icons.svg#arrow-up'/></svg></button>",
-				"<button name='down[$i]' value='1' title='" . h(lang('Move down')) . "' class='hidden light'><svg class='icon'><use href='static/icons.svg#arrow-down'/></svg></button>";
+			echo "<button name='add[$i]' value='1' title='" . h(lang('Add next')) . "' class='light'>", icon("add"), "</button>",
+				"<button name='up[$i]' value='1' title='" . h(lang('Move up')) . "' class='hidden light'>", icon("arrow-up"), "</button>",
+				"<button name='down[$i]' value='1' title='" . h(lang('Move down')) . "' class='hidden light'>", icon("arrow-down"), "</button>";
 		}
 		if ($orig == "" || support("drop_col")) {
-			echo "<button name='drop_col[$i]' value='1' title='" . h(lang('Remove')) . "' class='light'><svg class='icon'><use href='static/icons.svg#remove'/></svg></button>";
+			echo "<button name='drop_col[$i]' value='1' title='" . h(lang('Remove')) . "' class='light'>", icon("remove"), "</button>";
 		}
 		echo "</td>\n</tr>\n";
 	}