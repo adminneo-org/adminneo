b5ebdbe386e0973ef37285aca74fc123b21a9b76
Peter Knut <peter@pematon.com>
2025-07-14 22:44:42 +0200

    Defer syntax highlighting
    Inspired by: https://github.com/vrana/adminer/commit/b0e5c1d6e427e4b8ac4e7214465628d060f6f1f7

--- admin/core/Admin.php
+++ admin/core/Admin.php
@@ -76,7 +76,7 @@ class Admin extends Origin
 			"../vendor/vrana/jush/modules/jush-oracle.js",
 			"../vendor/vrana/jush/modules/jush-simpledb.js",
 			"../vendor/vrana/jush/modules/jush-js.js",
-		]));
+		]), true);
 	}
 
 	/**
@@ -1203,13 +1203,14 @@ class Admin extends Origin
 							$tablesColumns[$table][] = $field["field"];
 						}
 					}
-					echo "const autocompletion = jush.autocompleteSql('" . idf_escape("") . "', " . json_encode($tablesColumns) . ");\n";
+
+					echo "window.addEventListener('DOMContentLoaded', () => { autocompletion = jush.autocompleteSql('" . idf_escape("") . "', " . json_encode($tablesColumns) . "); });\n";
 				}
 
 				echo "</script>\n";
 			}
 
-			echo script("initSyntaxHighlighting('" . Connection::get()->getVersion() . "', " . (Connection::get()->isMariaDB() ? "true" : "false") . ", autocompletion);");
+			echo script("let autocompletion;\nwindow.addEventListener('DOMContentLoaded', () => { initSyntaxHighlighting('" . Connection::get()->getVersion() . "', " . (Connection::get()->isMariaDB() ? "true" : "false") . ", autocompletion); });");
 		}
 	}
 
