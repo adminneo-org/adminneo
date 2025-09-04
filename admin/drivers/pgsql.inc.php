vrana/adminer

PostgreSQL: Display auto_increment of inserted rows

adminer/drivers/pgsql.inc.php
@@ -241,6 +241,13 @@ if (isset($_GET["pgsql"])) {
 			$this->types[lang('User types')] = array_flip($types);
 		}
 
+		function insertSql($table, $set) {
+			$auto_increment = array_filter(fields($table), function ($field) {
+				return $field['auto_increment'];
+			});
+			return parent::insertSql($table, $set) . (count($auto_increment) == 1 ? " RETURNING " . idf_escape(key($auto_increment)) : "");
+		}
+
 		function insertUpdate($table, $rows, $primary) {
 			global $connection;
 			foreach ($rows as $set) {
@@ -781,7 +788,7 @@ ORDER BY SPECIFIC_NAME');
 	}
 
 	function last_id($result) {
-		return 0; // there can be several sequences
+		return (is_object($result) ? $result->fetch_column(0) : 0);
 	}
 
 	function explain($connection, $query) {

adminneo-org/adminneo

admin/drivers/pgsql.inc.php
@@ -361,6 +361,15 @@ if (isset($_GET["pgsql"])) {
 			$this->systemSchemas = ["information_schema", "pg_catalog", "pg_toast", "pg_temp_*", "pg_toast_temp_*"];
 		}
 
+		protected function insertSql(string $table, array $record): string
+		{
+			$autoIncrement = array_filter(fields($table), function ($field) {
+				return $field['auto_increment'];
+			});
+
+			return parent::insertSql($table, $record) . (count($autoIncrement) == 1 ? " RETURNING " . idf_escape(key($autoIncrement)) : "");
+		}
+
 		public function insertUpdate(string $table, array $records, array $primary): bool
 		{
 			foreach ($records as $record) {
@@ -906,7 +915,9 @@ ORDER BY conkey, conname") as $row) {
 
 	function last_id($result)
 	{
-		return 0; // there can be several sequences
+		$row = $result instanceof Result ? $result->fetchRow() : [];
+
+		return $row ? $row[0] : 0;
 	}
 
 	function explain(Connection $connection, string $query)