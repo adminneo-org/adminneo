vrana/adminer

PostgreSQL: Display description of system variables

adminer/variables.inc.php
@@ -9,10 +9,13 @@ if (!$variables) {
 	echo "<p class='message'>" . lang('No rows.') . "\n";
 } else {
 	echo "<table>\n";
-	foreach ($variables as $key => $val) {
+	foreach ($variables as $row) {
 		echo "<tr>";
+		$key = array_shift($row);
 		echo "<th><code class='jush-" . JUSH . ($status ? "status" : "set") . "'>" . h($key) . "</code>";
-		echo "<td>" . nl_br(h($val));
+		foreach ($row as $val) {
+			echo "<td>" . nl_br(h($val));
+		}
 	}
 	echo "</table>\n";
 }

adminneo-org/adminneo

admin/variables.inc.php
@@ -12,10 +12,13 @@ if (!$variables) {
 	echo "<p class='message'>" . lang('No rows.') . "\n";
 } else {
 	echo "<div class='scrollable'><table>\n";
-	foreach ($variables as $key => $val) {
+	foreach ($variables as $row) {
 		echo "<tr>";
+		$key = array_shift($row);
 		echo "<th><code class='jush-" . DIALECT . ($status ? "status" : "set") . "'>" . h($key) . "</code>";
-		echo "<td>" . nl2br(h($val));
+		foreach ($row as $val) {
+			echo "<td>" . nl2br(h($val));
+		}
 	}
 	echo "</table></div>\n";
 }