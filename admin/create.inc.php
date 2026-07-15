2eb6bd93a0af0605359a5a8e23e1e618815ad73b
Jakub Vrana <jakub@vrana.cz>
2026-07-15 08:38:53 +0200

    Create: Save settings after CSRF check

--- admin/create.inc.php
+++ admin/create.inc.php
@@ -28,7 +28,7 @@ if ($row["auto_increment_col"]) {
 	$row["fields"][$row["auto_increment_col"]]["auto_increment"] = true;
 }
 
-if ($_POST) {
+if ($_POST && !Admin::get()->getErrors()) {
 	Admin::get()->getSettings()->updateParameter("commentsOpened", $_POST["comments"] ?? null);
 }
 
