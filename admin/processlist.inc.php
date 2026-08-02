vrana/adminer

PostgreSQL 9.2+: Highlight queries in process list

adminer/processlist.inc.php
@@ -37,10 +37,10 @@ foreach (adminer()->processList() as $i => $row) {
 	}
 	echo "<tr>" . (support("kill") ? "<td>" . checkbox("kill[]", $row[JUSH == "sql" ? "Id" : "pid"], 0) : "");
 	foreach ($row as $key => $val) {
-		echo "<td>" . (
-			(JUSH == "sql" && $key == "Info" && preg_match("~Query|Killed~", $row["Command"]) && $val != "") ||
-			(JUSH == "pgsql" && $key == "current_query" && $val != "<IDLE>") ||
-			(JUSH == "oracle" && $key == "sql_text" && $val != "")
+		echo "<td>" . ($val != "" && (
+			(JUSH == "sql" && $key == "Info" && preg_match("~Query|Killed~", $row["Command"])) ||
+			(JUSH == "pgsql" && $key == "query") ||
+			(JUSH == "oracle" && $key == "sql_text"))
 			? "<code class='jush-" . JUSH . "' data-full='" . h($val) . "'>" . shorten_utf8($val, 100, "</code>")
 				. ' <a href="' . h(ME . ($row["db"] != "" ? "db=" . urlencode($row["db"]) . "&" : "") . "sql=" . urlencode($val)) . '">' . lang('Clone') . '</a>'
 				. ' <a href="" class="jsonly copy">🗐</a>'

adminneo-org/adminneo

admin/processlist.inc.php
@@ -39,10 +39,10 @@ foreach (process_list() as $i => $row) {
 	}
 	echo "<tr>" . (support("kill") ? "<td>" . checkbox("kill[]", $row[DIALECT == "sql" ? "Id" : "pid"], 0) : "");
 	foreach ($row as $key => $val) {
-		echo "<td>" . (
-			(DIALECT == "sql" && $key == "Info" && preg_match("~Query|Killed~", $row["Command"]) && $val != "") ||
-			(DIALECT == "pgsql" && $key == "current_query" && $val != "<IDLE>") ||
-			(DIALECT == "oracle" && $key == "sql_text" && $val != "")
+		echo "<td>" . ($val != "" && (
+			(DIALECT == "sql" && $key == "Info" && preg_match("~Query|Killed~", $row["Command"])) ||
+			(DIALECT == "pgsql" && $key == "query") ||
+			(DIALECT == "oracle" && $key == "sql_text"))
 			? "<code class='jush-" . DIALECT . "'>" . truncate_utf8($val, 100) . '</code> <a href="' . h(ME . ($row["db"] != "" ? "db=" . urlencode($row["db"]) . "&" : "") . "sql=" . urlencode($val)) . '">' . icon("edit") . lang('Clone') . '</a>'
 			: h($val)
 		);