vrana/adminer

SQLite: Show used and free space of the database
Sizes of single tables are available only through the dbstat virtual table
which reads every page of the database and is not compiled in by default
(e.g. the Windows PHP build lacks it). The database as a whole is free:
page_count and freelist_count are read from the header.

Data Length holds the used space including indexes because their pages
cannot be distinguished, Index Length stays empty.

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

adminer/db.inc.php
@@ -153,8 +153,11 @@ if (adminer()->homepage()) {
 			echo "<tr><td><th>" . lang('%d in total', count($tables_list));
 			echo "<td>" . h(JUSH == "sql" ? get_val("SELECT @@default_storage_engine") : "");
 			echo (collations() ? "<td>" . h(db_collation(DB, collations())) : '');
-			foreach (array("Data_length", "Index_length", "Data_free") as $key) {
-				echo ($columns[$key] ? "<td align='right' id='sum-$key'>" . ($full ? format_number($sums[$key]) : "") : "");
+			if ($full && function_exists('Adminer\db_status')) {
+				$sums = db_status();
+			}
+			foreach ($sums as $key => $sum) {
+				echo ($columns[$key] ? "<td align='right' id='sum-$key'>" . ($full ? format_number($sum) : "") : "");
 			}
 			echo "\n";
 

adminer/script.inc.php
@@ -27,6 +27,9 @@ if ($_GET["script"] == "db") {
 			}
 		}
 	}
+	if (function_exists('Adminer\db_status')) {
+		$sums = db_status();
+	}
 	foreach ($sums as $key => $val) {
 		json_row("sum-$key", format_number($val));
 	}

adminneo-org/adminneo

admin/db.inc.php
@@ -285,6 +285,9 @@ if ($_GET["ns"] === "") {
 		echo "<td><th>" . lang('%d in total', count($tables_list));
 		echo "<td>" . h(DIALECT == "sql" ? Connection::get()->getValue("SELECT @@default_storage_engine") : "");
 		echo ($db_collation != "" ? "<td>" . h($db_collation) : "");
+		if ($with_status && function_exists('AdminNeo\db_status')) {
+			$sums = db_status();
+		}
 		foreach ($sums as $key => $sum) {
 			echo "<td align='right' id='sum-$key'>" . ($with_status ? format_number($sum) : "");
 		}

admin/script.inc.php
@@ -42,6 +42,9 @@ if ($_GET["script"] == "db") {
 		}
 	}
 
+	if (function_exists('AdminNeo\db_status')) {
+		$sums = db_status();
+	}
 	foreach ($sums as $key => $val) {
 		$data["sum-$key"] = format_number($val);
 	}