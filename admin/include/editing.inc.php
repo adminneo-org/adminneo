vrana/adminer

Alter table: Add next column without page reload
Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

adminer/include/editing.inc.php
@@ -304,6 +304,7 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", array $f
 		echo (support("comment") ? "<td id='label-comment'$comment_class>" . lang('Comment') : "");
 	}
 	echo "<td>" . icon("plus", "add[" . (support("move_col") ? 0 : count($fields)) . "]", "+", lang('Add next'));
+	echo (support("move_col") ? "" : script("qsl('button').onclick = editingAddLastRow;"));
 	echo "</thead>\n<tbody>\n";
 	echo script("mixin(qsl('tbody'), {onclick: editingClick, onkeydown: editingKeydown, oninput: editingInput});");
 	foreach ($fields as $i => $field) {

adminneo-org/adminneo

admin/include/editing.inc.php
@@ -374,6 +374,7 @@ function edit_fields(array $fields, array $collations, $type = "TABLE", $foreign
 
 	echo "<td>";
 	echo "<button name='add[", (support("move_col") ? 0 : count($fields)), "]' value='1' title='", h(lang('Add next')), "' class='button light'>", icon_solo("add"), "</button>";
+	echo (support("move_col") ? "" : script("qsl('button').onclick = onAddLastFieldRowClick;"));
 	echo script("row_count = " . count($fields) . ";");
 	echo "</td>\n";
 