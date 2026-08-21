vrana/adminer

MySQL: Decode non-UTF-8 connect error as windows-1252
MySQL encodes error messages in character_set_results, which during
connect is still the handshake charset, so a non-English lc_messages on
a legacy charset yields raw 8-bit bytes. That charset is latin1 far more
often than cp1250, and MySQL latin1 is cp1252.

//IGNORE skips the bytes unassigned in cp1252 instead of failing the
whole conversion with a notice.

adminer/drivers/mysql.inc.php
@@ -205,7 +205,7 @@ if (!defined('Adminer\DRIVER')) {
 		static function connect(string $server, string $username, string $password) {
 			$connection = parent::connect($server, $username, $password);
 			if (is_string($connection)) {
-				if (function_exists('iconv') && !is_utf8($connection) && strlen($s = iconv("windows-1250", "utf-8", $connection)) > strlen($connection)) { // windows-1250 - most common Windows encoding
+				if (function_exists('iconv') && !is_utf8($connection) && strlen($s = iconv("windows-1252", "utf-8//IGNORE", $connection)) > strlen($connection)) { // windows-1252 - the same as MySQL latin1
 					$connection = $s;
 				}
 				return $connection;

adminneo-org/adminneo

admin/drivers/mysql.inc.php
@@ -579,7 +579,7 @@ if (isset($_GET["mysql"])) {
 		if (!$connection->openPasswordless($server, $username, $password, false)) {
 			$error = $connection->getError();
 
-			if (function_exists('iconv') && !is_utf8($error) && strlen($s = iconv("windows-1250", "utf-8", $error)) > strlen($error)) { // windows-1250 - most common Windows encoding
+			if (function_exists('iconv') && !is_utf8($error) && strlen($s = iconv("windows-1252", "utf-8//IGNORE", $error)) > strlen($error)) { // windows-1252 - the same as MySQL latin1
 				$error = $s;
 			}
 