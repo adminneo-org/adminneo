vrana/adminer

Use arrow functions instead of partialArg()
None of the functions passed to partialArg() used this, so the binding it
preserved was unnecessary. partial() stays because its targets rely on it.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

adminer/include/connect.inc.php
@@ -51,7 +51,7 @@ if (
 			$collations = collations();
 			echo "<form action='' method='post'>\n";
 			echo "<table class='checkable odds'>\n";
-			echo script("mixin(qsl('table'), {onclick: tableClick, ondblclick: partialArg(tableClick, true)});");
+			echo script("mixin(qsl('table'), {onclick: tableClick, ondblclick: event => tableClick(event, true)});");
 			echo "<thead><tr>"
 				. (support("database") ? "<td>" : "")
 				. "<th>" . lang('Database') . (get_session("dbs") !== null ? " - <a href='" . h(ME) . "refresh=1'>" . lang('Refresh') . "</a>" : "")

adminer/processlist.inc.php
@@ -20,7 +20,7 @@ page_header(lang('Process list'), $error);
 <div class="scrollable">
 <table class="nowrap checkable odds">
 <?php
-echo script("mixin(qsl('table'), {onclick: tableClick, ondblclick: partialArg(tableClick, true)});");
+echo script("mixin(qsl('table'), {onclick: tableClick, ondblclick: event => tableClick(event, true)});");
 // HTML valid because there is always at least one process
 $i = -1;
 foreach (adminer()->processList() as $i => $row) {

editor/db.inc.php
@@ -11,7 +11,7 @@ if (adminer()->homepage()) {
 	}
 	echo "<div class='scrollable'>\n";
 	echo "<table class='nowrap checkable odds'>\n";
-	echo script("mixin(qsl('table'), {onclick: tableClick, ondblclick: partialArg(tableClick, true)});");
+	echo script("mixin(qsl('table'), {onclick: tableClick, ondblclick: event => tableClick(event, true)});");
 	echo '<thead><tr class="wrap">';
 	echo '<td><input id="check-all" type="checkbox" class="jsonly">' . script("qs('#check-all').onclick = partial(formCheck, /^tables\[/);", "");
 	echo '<th>' . lang('Table');

adminneo-org/adminneo

admin/include/connect.inc.php
@@ -81,7 +81,7 @@ if (!(DB != "" ? Connection::get()->selectDatabase(DB) : isset($_GET["sql"]) || 
 			}
 
 			echo "</tbody>\n";
-			echo script("mixin(qsl('tbody'), {onclick: tableClick, ondblclick: partialArg(tableClick, true)});");
+			echo script("mixin(qsl('tbody'), {onclick: tableClick, ondblclick: event => tableClick(event, true)});");
 
 			echo "</table>\n";
 			echo "</div>\n"; // scrollable

admin/processlist.inc.php
@@ -52,7 +52,7 @@ foreach (process_list() as $i => $row) {
 
 if ($i >= 0) {
 	echo "</tbody>\n";
-	echo script("mixin(qsl('tbody'), {onclick: tableClick, ondblclick: partialArg(tableClick, true)});");
+	echo script("mixin(qsl('tbody'), {onclick: tableClick, ondblclick: event => tableClick(event, true)});");
 }
 
 echo "</table>\n";

editor/db.inc.php
@@ -46,7 +46,7 @@ foreach (table_status() as $table => $status) {
 }
 
 echo "</tbody>\n";
-echo script("mixin(qsl('tbody'), {onclick: tableClick, ondblclick: partialArg(tableClick, true)});");
+echo script("mixin(qsl('tbody'), {onclick: tableClick, ondblclick: event => tableClick(event, true)});");
 
 echo "</table>\n";
 echo "</div>\n";